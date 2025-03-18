da60a846f71eec89d9a543c926e99ad620ee0858
palaciosyost <palaciosyostin903@gmail.com>
2025-03-17 21:56:03 -0500

    redireccion al registro solucionado

--- model/sale.py
+++ model/sale.py
@@ -24,19 +24,18 @@ class SaleUnify(models.TransientModel):
     new_cliente_sale = fields.Many2one("res.partner", string="Nuevo Cliente")
     def unify_sale(self):
         sale_ids = self.env["sale.order"].browse(self.env.context.get("active_ids"))
-        print("DATOS DE SALE-ORDER")
-        print(sale_ids)
-        lineas = []
-        
+        new_cotizacion = ''
         if any( sale.state in ["fusionado", "sale", "cancel"] for sale in sale_ids):
             raise UserError(_("Las cotizaciones no deben estar en el estado  ['Fusionado', 'Orden de venta', 'Cancelado']"))
         else:
             if self.select_mode == "new_order":
-                self.new_order_merge(sale_ids,self.select_mode_cliente, self.new_cliente_sale )
+                new_cotizacion = self.new_order_merge(sale_ids,self.select_mode_cliente, self.new_cliente_sale )
             if self.select_mode == "delete_order":
-                self.delete_order(sale_ids,self.select_mode_cliente, self.new_cliente_sale )
+                new_cotizacion =  self.delete_order(sale_ids,self.select_mode_cliente, self.new_cliente_sale )
             if self.select_mode == "readonly_order":
-                self.new_readonly_order(sale_ids,self.select_mode_cliente, self.new_cliente_sale )
+                new_cotizacion = self.new_readonly_order(sale_ids,self.select_mode_cliente, self.new_cliente_sale )
+            if isinstance(new_cotizacion, dict):
+                return new_cotizacion
 
 
     def new_order_merge(self, sale_ids, select_mode_cliente, new_cliente_sale):
@@ -60,13 +59,19 @@ class SaleUnify(models.TransientModel):
 
 
         return {
-            "type" : "ir.actions.act_window",
-            "name" : "Cotizacion",
-            "res_model" : "sale.order",
-            "view_mode" : "form",
-            "res_id" : new_cotizacion.id,
-
+            "type": "ir.actions.act_window",
+            "name": _("Cotización"),
+            "res_model": "sale.order",
+            "view_mode": "form",
+            "res_id": new_cotizacion.id,  # Asegúrate de que `new_cotizacion` es un objeto `sale.order`
+            "target": "current",
+            "flags": {"mode": "edit"},
+            "views": [(self.env.ref("sale.view_order_form").id, "form")],
         }
+
+
+
+
     def new_readonly_order(self, sale_ids, select_mode_cliente, new_cliente_sale):
         if select_mode_cliente == 'cliente_set' and len(set(sale.partner_id.id for sale in sale_ids)) > 1:
             raise UserError(_("Si se requiere mantener el mismo cliente para la nueva cotizacion, seleccione las cotizaciones con que tengan el mismo cliente"))
@@ -89,15 +94,17 @@ class SaleUnify(models.TransientModel):
         for sale in sale_ids:
             sale.message_post(body=f"Esta cotizacion esta fusionada en la cotizacion {new_cotizacion.name}", message_type="comment", subtype_xmlid="mail.mt_note")
 
-
         return {
-            "type" : "ir.actions.act_window",
-            "name" : "Cotizacion",
-            "res_model" : "sale.order",
-            "view_mode" : "form",
-            "res_id" : new_cotizacion.id,
-
+            "type": "ir.actions.act_window",
+            "name": _("Cotización"),
+            "res_model": "sale.order",
+            "view_mode": "form",
+            "res_id": new_cotizacion.id,  # Asegúrate de que `new_cotizacion` es un objeto `sale.order`
+            "target": "current",
+            "flags": {"mode": "edit"},
+            "views": [(self.env.ref("sale.view_order_form").id, "form")],
         }
+
     
     def delete_order(self, sale_ids, select_mode_cliente, new_cliente_sale):
         if select_mode_cliente == 'cliente_set' and len(set(sale.partner_id.id for sale in sale_ids)) > 1:
@@ -117,10 +124,12 @@ class SaleUnify(models.TransientModel):
         sale_ids.unlink()
 
         return {
-            "type" : "ir.actions.act_window",
-            "name" : "Cotizacion",
-            "res_model" : "sale.order",
-            "view_mode" : "form",
-            "res_id" : new_cotizacion.id,
-
+            "type": "ir.actions.act_window",
+            "name": _("Cotización"),
+            "res_model": "sale.order",
+            "view_mode": "form",
+            "res_id": new_cotizacion.id,  # Asegúrate de que `new_cotizacion` es un objeto `sale.order`
+            "target": "current",
+            "flags": {"mode": "edit"},
+            "views": [(self.env.ref("sale.view_order_form").id, "form")],
         }
